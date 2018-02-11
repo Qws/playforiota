c3f24dca4919ac20d1908b52e1138cca30731c6b
deno <deno@hotmail.nl>
2018-02-12 00:25:41 +0100

    Shows IOTA balance of multiple addresses.

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 var IOTA = require("iota.lib.js");
 
 var iota = new IOTA({
-    'provider': 'http://eugeneoldisoft.iotasupport.com:14265'
+    'provider': 'http://nodes.iota.fm:80'
 });
 
-iota.api.getNodeInfo(function(error, success) {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(success);
+
+
+iota.api.getInputs("MTZTQLDLG9VRQ9OFJLQPNDQISUKZLCWTDOGJTZOJVZDZURVRPQMGIILEXMYCKFWABEROBESVMHVVBFDFD", function(error, response)
+{
+    if(error)
+    {
+        console.log(error);
     }
-});
+
+    else
+    {
+        console.log(response);
+        console.log();
+        for(var i = 0; (response["inputs"].length - 1) > i; i++)
+        {
+            console.log("IOTA balance of this specific address: ",response["inputs"][i]["balance"]);
+        }
+    }
+    console.log()
+})
 
 console.log(iota.version);
